844457631aed767d262de9d54ec8495d491b6d57
unknown <kanivision@gmail.com>
2021-07-25 13:16:42 +0530

    Folder Name

---
+++ src/org/samplegit/DataClass.java
@@ -0,0 +1,26 @@
+package org.samplegit;
+
+public class DataClass {
+	
+		private void empName() {
+			System.out.println("kani");
+
+		}
+		private void empAddress() {
+			System.out.println("chennai");
+
+		}
+		private void empphone() {
+			System.out.println("123456789");
+
+		}
+		public static void main(String[] args) {
+			DataClass s = new DataClass();
+			s.empName();
+			s.empAddress();
+			s.empphone();
+		}
+
+	}
+
+
